b076d2582c22c06d212a6b8e845ec0ad4d176649
Ahmed Hosam <keyjayningago@gmail.com>
2025-10-02 00:22:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ dotenv.config();
 
 app.use(exp.json());
 
+// Small CORS middleware to allow the frontend (including file:// origin) to access the API
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	if (req.method === 'OPTIONS') return res.sendStatus(200);
+	next();
+});
+
 mongo.connect(process.env.MONGO_URL).then(()=>console.log("DB connected")).catch(err=>console.log(err));
 
 app.use("/",doctorAPI);
 app.use("/",patientAPI);
 app.use("/",appointmentAPI);
 
-
-app.listen(process.env.PORT,()=>console.log("server is running on port 1080"));
+const PORT = process.env.PORT || 1080;
+app.listen(PORT, ()=>console.log(`server is running on port ${PORT}`));
